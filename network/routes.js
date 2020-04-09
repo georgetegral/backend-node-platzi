393fd422e7af1742b2360561cf992608f4c42361
georgetegral <georgetegral@gmail.com>
2020-04-09 13:27:21 -0500

    fixed minor bug

--- network/routes.js
+++ network/routes.js
@@ -4,9 +4,9 @@ const user = require('../components/user/network');
 const chat = require('../components/chat/network');
 
 const routes = function (server) {
-    server.use('/message', message);
-    server.use('/user', user);
-    server.use('/chat', chat);
+    server.use('/api/message', message);
+    server.use('/api/user', user);
+    server.use('/api/chat', chat);
 }
 
 module.exports = routes;
